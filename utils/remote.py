1544f728b100acfd791a1ead5adfd3e7a8a91c38
Your Name <you@example.com>
2022-06-07 15:08:56 +0200

    Added function to get host os name and version

--- utils/remote.py
+++ utils/remote.py
@@ -25,41 +25,22 @@ def get_app_version(user_name: str, ip_address: str, app_dir: str) -> str:
         return "failed_to_get_version"
 
     return "{0.stdout}".format(run_cmd).strip()
-
-
-def get_host_os_name_and_version(user_name: str, ip_address: str) -> str:
-    """gets version of operating system running on remote server
-
+    
+def get_os_details(user_name: str, ip_address: str) -> str:
+    """gets version of an application running on remote server
     Args:
         user_name (str): remote user name
         ip_address (str): ip address of remote server
-
     Returns:
-        str: version of operating system on remote server
+        str: version of application on remote server
     """
     try:
-
-        ssh = paramiko.SSHClient()
-        # ssh.load_host_keys('/home/username/.ssh/known_hosts')
-        # ssh.load_system_host_keys()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # Establish a connection with a hard coded pasword
-        # a private key will be used soon
-        ssh.connect(ip_address, username=user_name, password='PASSWORD')
-        # Enter the Linux command
-        stdin, stdout, stderr = ssh.exec_command("cat /etc/os-release")
-        # Output command execution results
-        result = stdout.read().splitlines()
-        # string of both os name and version
-        inputstring = f"{result[0]} {result[1]}"
-        # array of both os anme and version
-        collection = re.findall('"([^"]*)"', inputstring)
-        # Close the connection
-        ssh.close()
+        run_cmd = Connection(
+            f"{user_name}@{ip_address}").run(f"lsb_release -a", hide=True, echo=True)
+        print(run_cmd)
 
     except Exception as e:
         print(
-            f"--- Failed to get os_name and os_version for {ip_address} with exception: {e} ---")
-        return "failed_to_get_os_name_and_version"
-
-    return collection
+            f"--- Failed to get OS version for {ip_address} with exception: {e} ---")
+        return "failed_to_get_version"
+    return "{0.stdout}".format(run_cmd).strip()
