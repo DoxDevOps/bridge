f7c71281616e5dea99b632a1025752c7cafbc6b7
Dominic Kasanga <dominickasanga@gmail.com>
2023-01-14 01:00:20 +0200

    Update exporters.py

--- exporters.py
+++ exporters.py
@@ -19,11 +19,7 @@ def get_versions(ip_address: str, user_name: str, app_dirs: list, headers: dict)
             app_version = {"ip_address": ip_address,
                            "app_dir": app_dir, "version": version}
 
-          #             if not imp_exp_func.send_data(os.getenv('EXPORTER_ENDPOINT'), app_version, headers):
-
-          #                 return False
-
-            return True
+            imp_exp_func.send_data(os.getenv('EXPORTER_ENDPOINT'), app_version, headers)
 
 
 def ping_exporter(ip_address: str, headers: dict) -> bool:
