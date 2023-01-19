c2b87562ec926ef2fcbe25704b0c330c70876b10
dominic kasanga <dominickasanga@gmail.com>
2023-01-19 11:17:26 +0200

    modified:   utils/decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -5,8 +5,11 @@ from .net import host_is_reachable
 def check_if_host_is_reachable(func):
     @wraps(func)
     def decorated(*args, **kwargs):
-        if host_is_reachable(args[0]):
-            return func(*args, **kwargs)
-        else:
-            print("host is not reachable")
+        try:
+            if host_is_reachable(args[0]):
+                return func(*args, **kwargs)
+            else:
+                print("host is not reachable")
+        except:
+            print("error")
     return decorated
