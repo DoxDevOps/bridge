35b515d385fde21373ac28515da8985704a1ccb7
dominickasanga <dominickasanga@gmail.com>
2023-02-21 10:01:28 +0200

    modified:   bridge.py

--- bridge.py
+++ bridge.py
@@ -1,14 +1,16 @@
+import time
+import schedule
 from utils import imp_exp_func
 from exporters import get_host_details, get_versions, ping_exporter
 import os
 from dotenv import load_dotenv
 load_dotenv()
-import schedule
-import time
+
 
 def init():
     hosts = imp_exp_func.get_data_from_api(os.getenv('IMPORTER_ENDPOINT'))
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain','Authorization': os.getenv('EXPORTER_KEY')}
+    headers = {'Content-type': 'application/json',
+               'Accept': 'text/plain', 'Authorization': os.getenv('EXPORTER_KEY')}
     app_dirs = os.getenv('APP_DIRS').split(',')
 
     for host in hosts:
@@ -26,10 +28,12 @@ def init():
         # exports os details
         get_host_details(ip_address, user_name, headers)
 
-# run job every 3 hrs
-# for now
-init()
-schedule.every(2).hours.do(init)
-while True:
+
+if __name__ == '__main__':
+    # run job every 2 hrs
+    # for now
+    init()
+    schedule.every(2).hours.do(init)
+    while True:
         schedule.run_pending()
         time.sleep(1)
