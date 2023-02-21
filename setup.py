131d659e78775d116aa10e53eb46250e4913e036
dominickasanga <dominickasanga@gmail.com>
2023-02-21 09:54:44 +0200

    modified:   .gitignore 	new file:   setup.py 	new file:   tmp/bridge.service

---
+++ setup.py
@@ -0,0 +1,55 @@
+import os
+import os.path
+import sys
+
+SYSTEMD_SERVICE_TEMPLATE = '''
+[Unit]
+Description = bridge service
+After       = network.target
+
+[Service]
+WorkingDirectory={install_dir}
+ExecStart=/usr/bin/python3 {install_dir}/bridge.py
+
+# In case if it gets stopped, restart it immediately
+Restart     = always
+
+Type        = simple
+
+
+[Install]
+# multi-user.target corresponds to run level 3
+# roughtly meaning wanted by system start
+WantedBy    = multi-user.target
+'''
+
+
+def setup_autostart():
+    print('Setting up bridge autostart: tmp/bridge.service')
+    with open('tmp/bridge.service', 'w') as service_file:
+        service_file.write(
+            SYSTEMD_SERVICE_TEMPLATE.format(install_dir=os.getcwd()))
+
+    run('sudo systemctl stop bridge.service', die_on_fail=False)
+    run('sudo cp tmp/bridge.service /etc/systemd/system')
+    run('sudo systemctl enable bridge.service')
+    print('bridge has been set to automatically start up at boot time.')
+
+    run('sudo systemctl start bridge.service')
+
+
+def run(command, die_on_fail=True):
+    print('Running: {command}'.format(command=command))
+    if os.system(command) == 0:
+        return True
+
+    sys.stderr.write(
+        'error: Operation failed: {command}\n'.format(command=command))
+    if die_on_fail:
+        exit(255)
+
+    return False
+
+
+if __name__ == '__main__':
+    setup_autostart()
