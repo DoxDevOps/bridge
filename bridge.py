0f83242bb2afd28969aef557de1d5bdb8688199a
dominickasanga <dominickasanga@gmail.com>
2023-03-15 14:07:10 +0200

    modified:   bridge.py

--- bridge.py
+++ bridge.py
@@ -22,23 +22,23 @@ def init():
         user_name = host["fields"]["username"]
         site_name = host["fields"]["name"]
 
-        # create a new process instance
-        # exports versions of apps on the host
-        process_1 = Process(target=get_versions, args=(
-            ip_address, user_name, app_dirs, headers,))
-        # start the process
-        process_1.start()
-        # add the process to the list
-        processes.append(process_1)
+        # # create a new process instance
+        # # exports versions of apps on the host
+        # process_1 = Process(target=get_versions, args=(
+        #     ip_address, user_name, app_dirs, headers,))
+        # # start the process
+        # process_1.start()
+        # # add the process to the list
+        # processes.append(process_1)
 
-        # create a new process instance
-        # exports os details
-        process_2 = Process(target=get_host_details,
-                            args=(ip_address, user_name, headers,))
-        # start the process
-        process_2.start()
-        # add the process to the list
-        processes.append(process_2)
+        # # create a new process instance
+        # # exports os details
+        # process_2 = Process(target=get_host_details,
+        #                     args=(ip_address, user_name, headers,))
+        # # start the process
+        # process_2.start()
+        # # add the process to the list
+        # processes.append(process_2)
 
         # create a new process instance
         # exports sites system poc services
