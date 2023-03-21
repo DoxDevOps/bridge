e5b1376bd7e1cd3ee50838ce833601031a0d56ff
dominickasanga <dominickasanga@gmail.com>
2023-03-21 11:35:22 +0200

    modified:   utils/net.py

--- utils/net.py
+++ utils/net.py
@@ -3,6 +3,10 @@ import subprocess
 import pickle
 import os
 import paramiko
+import threading
+
+# create a lock object to synchronize access to the file
+lock = threading.Lock()
 
 
 def host_is_reachable(ip_address: str) -> bool:
@@ -26,31 +30,40 @@ def host_is_reachable(ip_address: str) -> bool:
 def updatePswrdDict(key, value):
     filename = 'utils/pswd_ao_dict.pickle'
 
-    # check if file exists and is not empty
-    if os.path.exists(filename) and os.path.getsize(filename) > 0:
-        # load the dictionary from file
-        with open(filename, 'rb') as f:
-            my_dict = pickle.load(f)
-    else:
-        # create an empty dictionary
-        my_dict = {}
+    # acquire the lock before accessing the file
+    with lock:
+        # check if file exists and is not empty
+        if os.path.exists(filename) and os.path.getsize(filename) > 0:
+            # load the dictionary from file
+            with open(filename, 'rb') as f:
+                my_dict = pickle.load(f)
+        else:
+            # create an empty dictionary
+            my_dict = {}
 
-    # update or insert a key-value pair
-    my_dict[key] = value
+        # update or insert a key-value pair
+        my_dict[key] = value
 
-    # save the updated dictionary to file
-    with open(filename, 'wb') as f:
-        pickle.dump(my_dict, f)
+        # save the updated dictionary to file
+        with open(filename, 'wb') as f:
+            pickle.dump(my_dict, f)
+            
+    # release the lock after accessing the file
 
 
 def getPswrd(ip_address):
     filename = 'utils/pswd_ao_dict.pickle'
-    # load the dictionary from file
-    with open(filename, 'rb') as f:
-        my_dict = pickle.load(f)
+
+    # acquire the lock before accessing the file
+    with lock:
+        # load the dictionary from file
+        with open(filename, 'rb') as f:
+            my_dict = pickle.load(f)
 
         # get the value for the given key, or return False if the key is not in the dictionary
-    value = my_dict.get(ip_address, False)
+        value = my_dict.get(ip_address, False)
+        
+    # release the lock after accessing the file
     if value is False:
         return False
     else:
