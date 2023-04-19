56d0ddb4100ea57b46497d1c825dbbb0682c76d2
dominickasanga <dominickasanga@gmail.com>
2023-04-19 11:01:04 +0200

    modified:   bridge.py

--- bridge.py
+++ bridge.py
@@ -22,51 +22,13 @@ def init():
         user_name = host["fields"]["username"]
         site_name = host["fields"]["name"]
 
-        # create a new process instance
-        # exports versions of apps on the host
-        process_1 = Process(target=get_versions, args=(
+        process_1 = Process(target=site_operations, args=(
             ip_address, user_name, site_name, app_dirs, headers,))
         # start the process
         process_1.start()
         # add the process to the list
         processes.append(process_1)
-
-        # create a new process instance
-        # exports os details
-        process_2 = Process(target=get_host_details,
-                            args=(ip_address, user_name, headers,))
-        # start the process
-        process_2.start()
-        # add the process to the list
-        processes.append(process_2)
-
-        # create a new process instance
-        # exports sites system poc services
-        process_3 = Process(target=check_poc_mysql_service,
-                            args=(ip_address, user_name, headers,))
-        # start the process
-        process_3.start()
-        # add the process to the list
-        processes.append(process_3)
-
-        # create a new process instance
-        # exports status for system service
-        process_4 = Process(target=check_poc_nginx_service,
-                            args=(ip_address, user_name, headers,))
-        # start the process
-        process_4.start()
-        # add the process to the list
-        processes.append(process_4)
-
-        process_5 = Process(target=net.save_failed_ping,
-                            args=(ip_address, user_name, site_name,))
-        process_5.start()
-        processes.append(process_5)
-
-        # exports results of a ping of host
-        # ping_exporter(ip_address, headers)
-
-    # wait for all processes to finish
+        # wait for all processes to finish
     for process in processes:
         process.join()
 
@@ -80,3 +42,53 @@ if __name__ == '__main__':
         runtime = end_time - start_time
         print("########################################################################################################")
         print("Runtime: ", runtime, " seconds")
+
+def site_operations(ip_address: str, user_name: str,  site_name: str, headers, app_dirs):
+    processes = []
+    # create a new process instance
+    # exports versions of apps on the host
+    process_1 = Process(target=get_versions, args=(
+        ip_address, user_name, site_name, app_dirs, headers,))
+    # start the process
+    process_1.start()
+    # add the process to the list
+    processes.append(process_1)
+
+    # create a new process instance
+    # exports os details
+    process_2 = Process(target=get_host_details,
+                        args=(ip_address, user_name, headers,))
+    # start the process
+    process_2.start()
+    # add the process to the list
+    processes.append(process_2)
+
+    # create a new process instance
+    # exports sites system poc services
+    process_3 = Process(target=check_poc_mysql_service,
+                        args=(ip_address, user_name, headers,))
+    # start the process
+    process_3.start()
+    # add the process to the list
+    processes.append(process_3)
+
+    # create a new process instance
+    # exports status for system service
+    process_4 = Process(target=check_poc_nginx_service,
+                        args=(ip_address, user_name, headers,))
+    # start the process
+    process_4.start()
+    # add the process to the list
+    processes.append(process_4)
+
+    process_5 = Process(target=net.save_failed_ping,
+                        args=(ip_address, user_name, site_name,))
+    process_5.start()
+    processes.append(process_5)
+
+    # exports results of a ping of host
+    # ping_exporter(ip_address, headers)
+    for process in processes:
+        process.join()
+
+    return True
