9fd92c0459dc0e13f469f8ae4a5f4e27d0c49178
dominickasanga <dominickasanga@gmail.com>
2023-04-19 11:02:49 +0200

    modified:   bridge.py

--- bridge.py
+++ bridge.py
@@ -7,42 +7,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
-def init():
-    hosts = imp_exp_func.get_data_from_api(os.getenv('IMPORTER_ENDPOINT'))
-    headers = {'Content-type': 'application/json',
-               'Accept': 'text/plain', 'Authorization': os.getenv('EXPORTER_KEY')}
-    app_dirs = os.getenv('APP_DIRS').split(',')
-    # define a list to keep track of all processes
-    processes = []
-
-    for host in hosts:
-
-        ip_address = host["fields"]["ip_address"]
-        user_name = host["fields"]["username"]
-        site_name = host["fields"]["name"]
-
-        process_1 = Process(target=site_operations, args=(
-            ip_address, user_name, site_name, app_dirs, headers,))
-        # start the process
-        process_1.start()
-        # add the process to the list
-        processes.append(process_1)
-        # wait for all processes to finish
-    for process in processes:
-        process.join()
-
-    return True
-
-
-if __name__ == '__main__':
-    start_time = time.time()
-    if init():
-        end_time = time.time()
-        runtime = end_time - start_time
-        print("########################################################################################################")
-        print("Runtime: ", runtime, " seconds")
-
 def site_operations(ip_address: str, user_name: str,  site_name: str, headers, app_dirs):
     processes = []
     # create a new process instance
@@ -92,3 +56,41 @@ def site_operations(ip_address: str, user_name: str,  site_name: str, headers, a
         process.join()
 
     return True
+
+
+def init():
+    hosts = imp_exp_func.get_data_from_api(os.getenv('IMPORTER_ENDPOINT'))
+    headers = {'Content-type': 'application/json',
+               'Accept': 'text/plain', 'Authorization': os.getenv('EXPORTER_KEY')}
+    app_dirs = os.getenv('APP_DIRS').split(',')
+    # define a list to keep track of all processes
+    processes = []
+
+    for host in hosts:
+
+        ip_address = host["fields"]["ip_address"]
+        user_name = host["fields"]["username"]
+        site_name = host["fields"]["name"]
+
+        process_1 = Process(target=site_operations, args=(
+            ip_address, user_name, site_name, app_dirs, headers,))
+        # start the process
+        process_1.start()
+        # add the process to the list
+        processes.append(process_1)
+        # wait for all processes to finish
+    for process in processes:
+        process.join()
+
+    return True
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    if init():
+        end_time = time.time()
+        runtime = end_time - start_time
+        print("########################################################################################################")
+        print("Runtime: ", runtime, " seconds")
+
+
